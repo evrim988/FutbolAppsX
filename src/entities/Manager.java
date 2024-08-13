18c68acb0a136deed4eddea09a7a30692ea9846b
alperenbcv <alperenbcv@gmail.com>
2024-08-13 17:56:11 +0300

    Latest Commit

---
+++ src/entities/Manager.java
@@ -0,0 +1,18 @@
+package entities;
+
+public class Manager extends Person {
+	private static Integer managerCounter=0;
+	
+	private String managerUserName;
+	private String managerPassword;
+	
+	public Manager() {
+		super(++managerCounter);
+	}
+	
+	public Manager(String name, Integer age, String nationality, String managerUserName, String managerPassword) {
+		super(++managerCounter, name, age, nationality);
+		this.managerUserName = managerUserName;
+		this.managerPassword = managerPassword;
+	}
+}
